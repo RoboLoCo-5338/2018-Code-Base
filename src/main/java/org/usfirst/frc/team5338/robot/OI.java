02f6d43d45ca7175974419eac72bdca8383f11c5
Arvind <loudjumpybolt@gmail.com>
2018-01-10 23:52:20 -0500

    Added comments to OI and Robot

--- src/main/java/org/usfirst/frc/team5338/robot/OI.java
+++ src/main/java/org/usfirst/frc/team5338/robot/OI.java
@@ -1,14 +1,20 @@
+//Package for all of our code.
 package org.usfirst.frc.team5338.robot;
 
+//Import of all essential wpilib classes.
 import edu.wpi.first.wpilibj.Joystick;
 
+//Main class that handles Operator Input.
 public class OI
 {
+	// Enum that represents all possible buttons in use.
 	public enum Button
 	{
 		SLOW, STRAIGHT
 	}
 
+	// Private method that returns a deadzone-adjusted value for a joystick value
+	// input.
 	private static double joystickDeadZone(final double value)
 	{
 		if(value > 0.05)
@@ -22,12 +28,12 @@ public class OI
 		return value;
 	}
 
+	// Creates private joystick objects for use.
 	private final Joystick joyL = new Joystick(0);
 	private final Joystick joyR = new Joystick(1);
 
-	public OI()
-	{
-	}
+	// Public method that returns the state of a particular button based on the
+	// Button enum.
 	public boolean get(final Button button)
 	{
 		switch(button)
@@ -40,6 +46,8 @@ public class OI
 				return false;
 		}
 	}
+	// Public method that returns the desired joystick object based on the int
+	// input.
 	public Joystick getJoystick(final int n)
 	{
 		if(n == 0)
@@ -55,10 +63,13 @@ public class OI
 			return null;
 		}
 	}
+	// Public method that returns the left joystick's deadzone-adjusted y-axis value
 	public double getLeft()
 	{
 		return OI.joystickDeadZone(this.joyL.getRawAxis(1));
 	}
+	// Public method that returns the right joystick's deadzone-adjusted y-axis
+	// value
 	public double getRight()
 	{
 		return OI.joystickDeadZone(this.joyR.getRawAxis(1));
