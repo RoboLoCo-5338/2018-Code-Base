7997c1f9bfae4b792cf79dab541d075aca03ad48
Arvind <loudjumpybolt@gmail.com>
2018-01-14 15:13:12 -0500

    Updated Vision to use Python 3

--- jetson/src/python/main.py
+++ jetson/src/python/main.py
@@ -1,28 +1,27 @@
 import cv2
 import numpy as np
-import thread
+from multiprocessing import Pool
 
-vid = cv2.VideoCapture("nvcamerasrc ! video/x-raw(memory:NVMM), width=(int)1280, height=(int)720,format=(string)I420, framerate=(fraction)60/1 ! nvvidconv flip-method=2 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink")
+cv2.setUseOptimized(True)
+vid = cv2.VideoCapture(0)
 
 lower_col, upper_col = np.array([20, 100, 100]), np.array([30, 255, 255])
 minw, minh = 125, 125
 
 frames = 0
 
-def process_frame(frame):
-    #HSV SEGMENTATION
+def process_frame(frame,frames):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     mask = cv2.inRange(hsv, lower_col, upper_col)
-    res = cv2.bitwise_and(frame,frame,mask=mask)
+    res = cv2.bitwise_and(frame, frame, mask=mask)
 
     empty, thresh = cv2.threshold(res, 150, 255, 0)
 
-    contim = cv2.cvtColor(thresh, cv2.COLOR_HSV2BGR)
-    contim = cv2.cvtColor(contim, cv2.COLOR_BGR2GRAY)
+    contim = np.array(thresh[:,:,2])
 
-    im2, contours, hierarchy = cv2.findContours(contim, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    
+    im2, contours, hierarchy = cv2.findContours(contim, mode=cv2.RETR_LIST, method=cv2.CHAIN_APPROX_SIMPLE)
+
     max_rect = None
     max_size = 0
     for index, c in enumerate(contours):
@@ -32,14 +31,16 @@ def process_frame(frame):
             if w*h > max_size:
                 max_rect = rect
     if max_rect is not None:
-       x, y, w, h = max_rect
-       print(((x + w/2, y + h/2),(w, h)))
+        x, y, w, h = max_rect
+        print("CRD: {} {}".format(repr(((x + w/2, y + h/2),(w, h))),frames))
     else:
-       print(None)
+        print("CRD: {} {}".format(None,frames))
 
 
 while(True):
     ret, frame = vid.read()
-    thread.start_new_thread(process_frame,(frame,))
+    frames += 1
+    with Pool(processes=4) as pool:
+        pool.apply_async(process_frame,(frame,frames))
 
 vid.release()
