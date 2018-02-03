81d5b65d6d0e8e8d9bbfd6902a8d330285a46df9
Siddharth <Siddharth@LAPTOP-KQUPV5KM>
2018-02-03 17:33:24 -0500

    Commented the code for clarity and added a statement to exit the subsystem if a talon wasn't working. TBD: sensing if a talon is working or not.

--- src/main/java/org/usfirst/frc/team5338/robot/subsystems/ClawTilter.java
+++ src/main/java/org/usfirst/frc/team5338/robot/subsystems/ClawTilter.java
@@ -11,10 +11,11 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ClawTilter extends Subsystem
 {
-	private final WPI_TalonSRX dartTalon = new WPI_TalonSRX(31);
-	private double potValue;
-	final double retracted = -190;
-	final double extended = -126;
+	private final WPI_TalonSRX dartTalon = new WPI_TalonSRX(31); //talon connected to the dart actuator
+	private double potValue; //potentiometer value
+	final double retracted = -190; // potentiometer value when the dart actuator is retracted
+	final double extended = -126; //potentiometer value when dart actuator is extended
+	boolean working;
 	
 	public ClawTilter()
 	{
@@ -28,37 +29,43 @@ public class ClawTilter extends Subsystem
 	}
 	public void tilt(final OI oi)
 	{
-		this.potValue = this.dartTalon.getSensorCollection().getAnalogIn();
-		if(Robot.oi.get(OI.Button.RAISE))
+		this.potValue = this.dartTalon.getSensorCollection().getAnalogIn(); //sense potentiometer value
+		working = true; // change to talon sensing
+		if(!working) { //if the talon isn't working
+			
+		}
+		else if(Robot.oi.get(OI.Button.RAISE)) // raise the arm (extends the actuator)
 		{
-			if(this.potValue <= this.extended)
-			{ // extend
-				final double difference = Math.abs(this.potValue - this.extended);
-				final int distance = 15;
+			if(this.potValue <= this.extended) // if the actuator isn't fully extended
+			{ // we slow the actuator at the end to prevent a hard stop
+				final double difference = Math.abs(this.potValue - this.extended); //how much further does the actuator need to extend
+				final int distance = 15; //how soon should the actuator begin to slow down (while extending)
+				final double lowSpeed = 0.10; //the speed to which the actuator will slow down to
 				if(difference <= distance)
 				{
-					final double speedDifference = (0.90 - 0.10) / distance;
+					final double speedDifference = (0.90 - lowSpeed) / distance;//this code slows the speed in a linear fashion
 					this.dartTalon.set(0.90 - (speedDifference * (distance - difference)));
 				}
 				else
 				{
-					this.dartTalon.set(0.90);
+					this.dartTalon.set(0.90); //extend at a fast speed
 				}
 			}
 			else
 			{
-				this.dartTalon.set(0);
+				this.dartTalon.set(0); //turn off the actuator if it has gone past the limit. Prevents jamming.
 			}
 		}
-		else if(Robot.oi.get(OI.Button.LOWER))
-		{
-			if(this.potValue >= this.retracted)
-			{ // retract
+		else if(Robot.oi.get(OI.Button.LOWER)) // lower the arm (retracts the actuator)
+		{ //this code mirrors the code above that is used for extending
+			if(this.potValue >= this.retracted) // if the actuator isn't fully retracted
+			{
 				final double difference = Math.abs(this.potValue - this.retracted);
-				final int distance = 27;
+				final int distance = 27;  //how soon should the actuator begin to slow down (while retracting)
+				final double lowSpeed = 0.10;
 				if(difference <= distance)
 				{
-					final double speedDifference = (0.90 - 0.10) / distance;
+					final double speedDifference = (0.90 - lowSpeed) / distance;
 					this.dartTalon.set(-(0.90 - (speedDifference * (distance - difference))));
 				}
 				else
@@ -71,10 +78,10 @@ public class ClawTilter extends Subsystem
 				this.dartTalon.set(0);
 			}
 		}
-		else
+		else // if we aren't extending or retracting, stop the motor
 		{
 			this.dartTalon.set(0);
 		}
-		SmartDashboard.putNumber("pot", this.potValue);
+		SmartDashboard.putNumber("pot", this.potValue); // display the potentiometer value in case the code needs to be fine tuned 
 	}
 }
