101db49c316d60bebe7458a2de6bc60fea3ed95b
Arvind <loudjumpybolt@gmail.com>
2018-02-04 15:04:50 -0500

    Finished child proofing code that is ready for use on Friday demonstration

--- robot/src/main/java/org/usfirst/frc/team5338/robot/subsystems/DriveTrain.java
+++ robot/src/main/java/org/usfirst/frc/team5338/robot/subsystems/DriveTrain.java
@@ -15,8 +15,8 @@ public class DriveTrain extends Subsystem
 {
 	private final WPI_TalonSRX DRIVEL1 = new WPI_TalonSRX(1);
 	public final WPI_TalonSRX DRIVEL2 = new WPI_TalonSRX(4);
-	public final WPI_TalonSRX DRIVER1 = new WPI_TalonSRX(2);
-	public final WPI_TalonSRX DRIVER2 = new WPI_TalonSRX(3);
+	public final WPI_TalonSRX DRIVER1 = new WPI_TalonSRX(3);
+	public final WPI_TalonSRX DRIVER2 = new WPI_TalonSRX(2);
 	private final SpeedControllerGroup m_left = new SpeedControllerGroup(this.DRIVEL1, this.DRIVEL2);
 	private final SpeedControllerGroup m_right = new SpeedControllerGroup(this.DRIVER1, this.DRIVER2);
 	private final DifferentialDrive DRIVE = new DifferentialDrive(this.m_left, this.m_right);
@@ -36,7 +36,7 @@ public class DriveTrain extends Subsystem
 	}
 	public void drive(final OI oi)
 	{
-		this.DRIVE.tankDrive(oi.getRight() / 3.0, oi.getLeft() / 3.0, false);
+		this.DRIVE.tankDrive(oi.getRight() / 3.5, oi.getLeft() / 3.5, false);
 	}
 	@Override
 	public void initDefaultCommand()
