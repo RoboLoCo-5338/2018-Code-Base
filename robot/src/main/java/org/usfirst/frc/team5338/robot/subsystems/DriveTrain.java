5da8355744eff09c7d9b977964014e68dcb1f907
Arvind <loudjumpybolt@gmail.com>
2018-02-04 13:59:54 -0500

    Fixed motion so that it at least just moves in basic tank

--- robot/src/main/java/org/usfirst/frc/team5338/robot/subsystems/DriveTrain.java
+++ robot/src/main/java/org/usfirst/frc/team5338/robot/subsystems/DriveTrain.java
@@ -5,23 +5,30 @@ import org.usfirst.frc.team5338.robot.commands.JoystickDrive;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class DriveTrain extends Subsystem
 {
-	private final WPI_TalonSRX DRIVEL1 = new WPI_TalonSRX(4);
-	public final WPI_TalonSRX DRIVEL2 = new WPI_TalonSRX(3);
+	private final WPI_TalonSRX DRIVEL1 = new WPI_TalonSRX(1);
+	public final WPI_TalonSRX DRIVEL2 = new WPI_TalonSRX(4);
 	public final WPI_TalonSRX DRIVER1 = new WPI_TalonSRX(2);
-	public final WPI_TalonSRX DRIVER2 = new WPI_TalonSRX(1);
+	public final WPI_TalonSRX DRIVER2 = new WPI_TalonSRX(3);
 	private final SpeedControllerGroup m_left = new SpeedControllerGroup(this.DRIVEL1, this.DRIVEL2);
 	private final SpeedControllerGroup m_right = new SpeedControllerGroup(this.DRIVER1, this.DRIVER2);
 	private final DifferentialDrive DRIVE = new DifferentialDrive(this.m_left, this.m_right);
+	private final Compressor driveCompressor = new Compressor(5);
+	private final DoubleSolenoid driveSolenoid = new DoubleSolenoid(5, 0, 1);
 	
 	public DriveTrain()
 	{
 		super();
+		this.driveCompressor.setClosedLoopControl(true);
+		this.driveCompressor.start();
+		this.driveSolenoid.set(DoubleSolenoid.Value.kForward);
 	}
 	public void drive(final double left, final double right)
 	{
@@ -29,7 +36,7 @@ public class DriveTrain extends Subsystem
 	}
 	public void drive(final OI oi)
 	{
-		this.DRIVE.tankDrive(oi.getLeft() / 4.0, oi.getRight() / 4.0, true);
+		this.DRIVE.tankDrive(oi.getLeft(), oi.getRight(), true);
 	}
 	@Override
 	public void initDefaultCommand()
